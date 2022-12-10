f22997a75557b033e7ae53b564d72ce10a953651
Alwin Vk <alwinvarghesek@gmail.com>
2022-12-10 15:17:21 +0000

    Initial Commit

---
+++ ShoppingUI/ShoppingUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingUIApp.swift
+//  ShoppingUI
+//
+//  Created by Alwin VK on 10/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
